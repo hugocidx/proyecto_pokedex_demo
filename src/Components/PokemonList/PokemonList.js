e8ef2f8ae05634246d1d0885a4a0657895fbc729
hugo <hugocidx@gmail.com>
2022-10-10 13:56:22 -0300

    Merge "branch fixbug"

--- src/Components/PokemonList/PokemonList.js
+++ src/Components/PokemonList/PokemonList.js
@@ -3,17 +3,16 @@ import "./PokemonList.scss";
 import { getListPokemon, getDetailPokemon } from "../../Api/Pokemon";
 import Card from "../Cards/Card";
 
-const PokemonList = ({listaProperty}) => {
-  return (
-    listaProperty != undefined && listaProperty.length > 0 ? 
-      <div className="content-cards">
-        {
-          listaProperty.map((pokemonDetail,index)=>
-            <Card pokemon={pokemonDetail} key={index} />
-          )
-        }
-      </div>
-    : <h1>No tengo info para pintar</h1>
+const PokemonList = ({ listaProperty }) => {
+  return listaProperty != undefined && listaProperty.length > 0 ? (
+    <div className="content-cards">
+      {listaProperty.map((pokemonDetail, index) => (
+        <Card pokemon={pokemonDetail} key={index} />
+      ))}
+    </div>
+  ) : (
+    <h1>No tengo info para pintar</h1>
   );
 };
+
 export default PokemonList;
